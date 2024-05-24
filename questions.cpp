a5a69827a0ff94f59cbe6227f438a7e3b2f4fb30
Ishita <ishitajain385@gmail.com>
2024-05-24 22:49:05 +0530

    sixth ques uploaded

--- questions.cpp
+++ questions.cpp
@@ -89,25 +89,52 @@ using namespace std;
 
 //   You must write an algorithm with O(log n) runtime complexity.                                         
 
-int searchInsert(vector<int>& nums, int target) {
+// int searchInsert(vector<int>& nums, int target) {
        
-    int start= 0;
-  int end= nums.size() -1;
-    while(start<=end)   {
+//     int start= 0;
+//   int end= nums.size() -1;
+//     while(start<=end)   {
+//         int mid= start+(end-start)/2;
+//         if(nums[mid]==target){
+//             return mid;
+//              }
+//         else if(nums[mid]>target) {
+//             end= mid-1;
+//              }
+//         else {
+//       start= mid+1;
+//       }
+//        }
+// return start;
+//     }
+
+                                        // QUESTION 6
+
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+   int search(vector<int>& nums, int target) {
+     int start= 0;
+     int end= nums.size() -1;
+     while(start<=end)  {
         int mid= start+(end-start)/2;
-        if(nums[mid]==target){
+        if(nums[mid]==target) {
             return mid;
-             }
+        }
         else if(nums[mid]>target) {
-            end= mid-1;
-             }
-        else {
-      start= mid+1;
-      }
-       }
-return start;
+            end=mid-1;
+
+        }
+        else{
+            start=mid+1;
+        }
+     }
+     return -1;
     }
-    
+
+
+
 int main() {
     
 }
