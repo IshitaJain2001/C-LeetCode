a020a8799ff8682e94144d259f9f2d26fbd55194
Ishita <ishitajain385@gmail.com>
2024-05-24 22:43:24 +0530

    uploaded fifth question

--- questions.cpp
+++ questions.cpp
@@ -72,17 +72,42 @@ using namespace std;
 
 // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array. 
 
-     int missingNumber(vector<int>& nums) {
-     int ans=0;
-     int n= nums.size();
-     for(int i=0; i<n; i++) {
-     ans += nums[i];
-      }
-     int expected= n*(n+1)/2;
-     int finalans= expected-ans;
-     return finalans;
-    };
+    //  int missingNumber(vector<int>& nums) {
+    //  int ans=0;
+    //  int n= nums.size();
+    //  for(int i=0; i<n; i++) {
+    //  ans += nums[i];
+    //   }
+    //  int expected= n*(n+1)/2;
+    //  int finalans= expected-ans;
+    //  return finalans;
+    // };
 
+                                            // QUESTION 5
+
+//    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+//   You must write an algorithm with O(log n) runtime complexity.                                         
+
+int searchInsert(vector<int>& nums, int target) {
+       
+    int start= 0;
+  int end= nums.size() -1;
+    while(start<=end)   {
+        int mid= start+(end-start)/2;
+        if(nums[mid]==target){
+            return mid;
+             }
+        else if(nums[mid]>target) {
+            end= mid-1;
+             }
+        else {
+      start= mid+1;
+      }
+       }
+return start;
+    }
+    
 int main() {
     
 }
